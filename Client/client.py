1776c06f0010f4edaba965c780da55497498c71a
micahmeadows <micahstlp@gmail.com>
2022-02-17 17:52:28 -0500

    Add message handlers

--- Client/client.py
+++ Client/client.py
@@ -1,23 +1,24 @@
-import sys
-sys.path.append(sys.path[0] + '\..')
-
-import asyncio
 import websockets
+import asyncio
+import sys
+sys.path.append(sys.path[0] + '/..')
 
 from Shared.Message import ClientSetNameMessage
 
-async def hello():
+async def listenForMessages(websocket):
+    async for message in websocket:
+        print(f"Received message: {message}")
+        await websocket.send(message)
+
+
+async def connectToServer():
     async with websockets.connect('ws://localhost:9292') as websocket:
 
         name = input("What's your name? ")
         nameMessage = ClientSetNameMessage(name)
 
-        await websocket.send(nameMessage)
-        # print("> {}".format(name))
+        await websocket.send(nameMessage.toJSON())
 
-        greeting = await websocket.recv()
-        # print("< {}".format(greeting))
+        asyncio.get_event_loop().run_until_complete(await listenForMessages(websocket))
 
-asyncio.get_event_loop().run_until_complete(hello())
-# C:\Git\socketrpg\Shared\Messages\Message.py
-# Shared\Messages\Message.py
+asyncio.get_event_loop().run_until_complete(connectToServer())
