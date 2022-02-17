ab88620c77ab94c35db7c64b8edd6c669438d54c
johnwhh <johnwhh@icloud.com>
2022-02-17 16:38:00 -0500

    Add game logic

--- Server/server.py
+++ Server/server.py
@@ -1,19 +1,25 @@
 import asyncio
 import websockets
+from game import Game
+
 
 class Server:
-  async def handler(self, websocket, path):
-    name = await websocket.recv()
-    print("{} has connected.".format(name))
+    def __init__(self):
+        self.game = Game()
 
-    greeting = "Hello {}!".format(name)
-    await websocket.send(greeting)
+    async def _handleInput(self, websocket, path):
+        name = await websocket.recv()
+        print("{} has connected.".format(name))
 
-  async def start(self):
-    runGameLoop = websockets.serve(self.handler, 'localhost', 9292)
+        greeting = "Hello {}! You have successfully connected.".format(name)
+        await websocket.send(greeting)
 
-    asyncio.get_event_loop().run_until_complete(runGameLoop)
-    asyncio.get_event_loop().run_forever()
+    def start(self):
+        runGameLoop = websockets.serve(self._handleInput, 'localhost', 9292)
+
+        asyncio.get_event_loop().run_until_complete(runGameLoop)
+        asyncio.get_event_loop().run_forever()
+
 
 server = Server()
 server.start()
